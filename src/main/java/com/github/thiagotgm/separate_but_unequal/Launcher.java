dc549a7a2c4ae1300d26cf9bca44e1b40c8f5332
ThiagoTGM <thiagotgmgaming@gmail.com>
2017-05-23 09:11:46 -0700

    Project start.
    Maven set up. Laucher class created.

---
+++ src/main/java/com/github/thiagotgm/separate_but_unequal/Launcher.java
@@ -0,0 +1,17 @@
+package com.github.thiagotgm.separate_but_unequal;
+
+/**
+ * Class that launches the game on startup.
+ *
+ * @version 1.0
+ * @author Thiago
+ * @since 2017-05-23
+ */
+public class Launcher {
+
+    public static void main( String[] args ) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
